533d9639621e41fcb425683d8f6e96f904532c7b
mihara3 <ki85ko@gmail.com>
2024-07-02 17:38:19 +0900

    HoT: initial revision

---
+++ game.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+    int i, heads = 0, tails = 0, a;
+    printf("Tossing a coin...\n");
+    for(i = 0; i < 3; i++){
+        printf("Round %d: ", i+1);
+        srand((unsigned int)time(NULL)); // create rand based on time
+        a = rand();
+        if(a%2 == 0){
+            printf("Heads\n");
+            heads++;
+        }
+        else{
+            printf("Tails\n");
+            tails++;
+        }
+        if(i < 2) sleep(1); // wait 1 second
+    }
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+    return 0;
+}
